17c45b7c2895eac5d1e9eb1f97d1478b71faf829
SatishEddhu <et_satish@hotmail.com>
2016-12-06 15:09:02 +0530

    Added covariance notes; updated comments in restaurant_regression_cart_ensemble

---
+++ covariance.R
@@ -0,0 +1,30 @@
+library(ggplot2)
+
+stock_plot = function(s1, s2) {
+  df = data.frame(a=s1,b=s2)
+  
+  ggplot(df) + geom_point(aes(x=a, y=b))
+  print(cov(df$a, df$b))  # covariance
+  print(cor(df$a, df$b))  # correlation
+}
+
+s1 = c(100,200,300,400)
+s2 = c(10,20,30,50)
+s3 = c(1,2,3,5)
+s4 = c(500,600,700,800)
+s5 = c(5,3,2,1)
+s6 = c(800,700,600,500)
+
+X11()
+df = data.frame(s1,s2)
+ggplot(df) + geom_point(aes(s1, s2))
+
+# covariance depends on the scale of the values. So, even if there is the same linear relationship,
+# covariance values will change. So, normalize the values by z-score and compute covariance on the
+# z-scores; this is called correlation and its value range is [-1, +1].
+# For z-score, almost 99% values fall in the [-3, +3] value range
+stock_plot(s1,s2)
+stock_plot(s1,s3)
+stock_plot(s1,s4)
+stock_plot(s1,s5)
+stock_plot(s1,s6)
--- restaurant/restaurant_regression_cart_ensemble.R
+++ restaurant/restaurant_regression_cart_ensemble.R
@@ -32,72 +32,29 @@ cart_model = train(revenue ~ ., restaurant_train1, method="rpart", trControl=res
 # do not set the "importance" flag for rpart. Setting it to TRUE or FALSE both give error.
 varImp(cart_model)
 # Although cp = 0, we have pre-puned based on the default numSplits=20 (min. num of nodes in leaf to split)
-# We see that the most important variables are not necessarily the ones at the top; some other approach used
+
+# We see that the most important variables are not necessarily the ones at the top; another approach used
+# Important variables are computed by permuting a variable's column values in test data, and
+# re-calculating the accuracy of test data. If accuracy goes down, the variable is important.
+
+# CART uses a greedy approach to split at each level; hence the top values are not necessarily
+# the most important.
 cart_model$finalModel
 
 cart_grid = expand.grid(.cp=0)
-#  Gives error. "wrong model type for regression". Perhaps cannot do meaningful post-pruning for C4.5
+#  Gives error. "wrong model type for regression".
+
 # C4.5 cannot be used for 'regression'. It can be used only for 'classification'. "C4.5 - C only for classification"
+# The post-pruning strategy in C4.5 (which does not use the training data) cannot be applied on
+# 'continuous' target variables. Hence C4.5 cannot be used for regression.
 c4.5_model = train(revenue ~ ., restaurant_train1, method="J48", trControl=resample_strategy)
 
 set.seed(100)
 resample_strategy = trainControl(method="boot")
 treebag_model = train(revenue ~ ., restaurant_train1, method="treebag", trControl=resample_strategy)
-treebag_model # performance not great since trees are related; most use P28, num_days as most important attributes
+# performance not great since trees are related; most use P28, num_days as most important attributes
+treebag_model
 # Diff trees show diff variable importance because they are built on different random samples
 treebag_model$finalModel$mtrees[[1]]
 treebag_model$finalModel$mtrees[[2]]
 treebag_model$finalModel$mtrees[[15]]
-
-
-#############################
-# IGNORE THE BELOW SECTION  #
-#############################
-
-
-# Repeat all the above 'feature engineering' work on 'test' data also
-restaurant_test = read.csv("test.csv", header = TRUE, na.strings=c("NA",""), stringsAsFactors = TRUE)
-dim(restaurant_test)
-str(restaurant_test)
-restaurant_test$Open.Date = as.character(restaurant_test$Open.Date)
-
-# No need to add level "MB" in test data as it is already present here
-levels(restaurant_test$Type)
-
-restaurant_test$num_days = as.Date(c("31-12-2014"), format="%d-%m-%Y") - as.Date(restaurant_test$Open.Date, format="%m/%d/%Y")
-restaurant_test$num_days = as.numeric(restaurant_test$num_days)
-
-# revenue not present in test data; standardize all distances just like we did in 'train'
-restaurant_test1 = restaurant_test[-c(1,2,3)]
-
-restaurant_test2 = as.data.frame(predict(dummyVarsObj, restaurant_test1))
-dim(restaurant_test2)
-str(restaurant_test2)
-
-restaurant_test3 = predict(preObj, restaurant_test2)
-dim(restaurant_test3)
-str(restaurant_test3)
-
-# Predict the revenue using the standardized distances
-restaurant_test3$Prediction = predict(knn_model, restaurant_test3)
-restaurant_test3$Id = restaurant_test$Id
-
-# Kaggle RMSE score is still only 1974545; implies that 'num_days/Open Date' was not so helpful afterall!
-result = restaurant_test3[,c("Id", "Prediction")]
-write.csv(result, "submission_v3.csv", row.names = F)
-
-# How do we get the intuition on what attributes might be important for prediction?
-# Build decision tree/forest first. Attributes that branch at the top of the tree are more important
-
-resample_strategy = trainControl(method="boot", number=10)
-set.seed(100)
-rf_model = train(restaurant_train[,-c(1,2,3,43)], restaurant_train$revenue, method="rf", trControl=resample_strategy, importance=TRUE)
-dim(restaurant_train)
-str(restaurant_train)
-
-# This shows that P28,P6 are the most important attributes. num_days is only 10th most important.
-# City.Group is 12th and Type is 18th. This explains why our prediction did not improve by using
-# these attributes. It is also bad that our kNN model gives equal weightage to all the attributes!
-varImp(rf_model)
-x11()
-varImpPlot(rf_model$finalModel)
