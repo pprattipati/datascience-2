0367b182d3042af652b2d9a6120a7413fe6bead5
pprattipati <prasannaperf@gmail.com>
2016-12-07 18:57:17 +0530

    Update dummy-variables.R

--- dummy-variables.R
+++ dummy-variables.R
@@ -4,6 +4,7 @@ pclass = factor(c("1","2","3"))
 passengers = data.frame(age, gender, pclass)
 str(passengers)
 # dummy variables are used to convert 'factors' to 'continuous' variables
+##dummy variables will Convert variables from factors to discrete.. 
 # variables that are already 'continuous' remain unchanged
 tmp = dummyVars(~age+gender+pclass, passengers)
 # use information to predict the continuous variables
